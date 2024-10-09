e82160bbe49ddb23bdf09f5383102e63c1cdf9b7
Saircasticc <pathurisai31@gmail.com>
2024-10-09 23:31:00 +0530

    new commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import App from './App'
-import Home from './components/Pages/Home'
-import ForecastPage from './components/Pages/ForecastPage'
-import WeatherMap from './components/Pages/WeatherMap'
-import WeatherNews from './components/Pages/WeatherNews'
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App/>}>
-      <Route path='' element={<Home />} />
-      <Route path='forecast' element={<ForecastPage />} />
-      <Route path='map' element={<WeatherMap />} />
-      <Route path='news' element={<WeatherNews />} />
-    </Route>
-  )
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
+import Home from './components/Pages/Home';
+import ForecastPage from './components/Pages/ForecastPage';
+import WeatherMap from './components/Pages/WeatherMap';
+import WeatherNews from './components/Pages/WeatherNews';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route index element={<Home />} />
+          <Route path='forecast' element={<ForecastPage />} />
+          <Route path='map' element={<WeatherMap />} />
+          <Route path='news' element={<WeatherNews />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
+);
